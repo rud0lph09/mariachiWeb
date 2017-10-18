9d34dbaa216104a09af213c706e7c2c4242ff716
joseangelchepo <chepo_nr@comunidad.unam.mx>
2017-10-18 13:19:22 -0500

    Init

---
+++ src/App.vue
@@ -0,0 +1,147 @@
+<template>
+  <div id="app">
+    <div class="container text_mariachi">
+      <div class="row">
+        <div class="col-lg-6 col-12">
+          <img id="logo_mariachi" src="./assets/logo_mariachi.png" alt="logo">
+          <h1>Nos estamos renovando</h1>
+          <h2>Llevaremos tus ideas al siguiente nivel,</h2>
+          <p>utilizando las tecnologías de desarrollo más novedosas podremos diseñarte una solucion adecuada.</p>
+        </div>
+        <div class="col-lg-6 col-12">
+          <div class="row logos">
+            <div class="col-sm-12">
+              <h3>Nuestros clientes</h3>
+              <div class="row">
+                <div class="col-lg-4">
+                  <img src="./assets/allianz_logo.png" alt="logo">
+                </div>
+                <div class="col-lg-4">
+                  <img src="./assets/gsalinas_logo.png" alt="logo">
+                </div>
+                <div class="col-lg-4">
+                  <img src="./assets/traveo_logo.png" alt="logo">
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-12">
+              <h3>Golden Partners</h3>
+              <div class="row">
+                <div class="col-lg-4"></div>
+                <div class="col-lg-4">
+                  <img src="./assets/oracle_logo.png" alt="logo">
+                </div>
+                <div class="col-lg-4"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <h6 id="derechos">© Derechos reservados 2017</h6>
+      <h6 id="correo">hola@mariachi.io</h6>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Exo:300,900|Montserrat');
+#app {
+  font-family: 'Exo', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+
+body {
+    background-image: url("assets/Background_image.jpg");
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.text_mariachi {
+  max-width: 95%;
+  margin-top: 5%;
+  text-align: left;
+}
+#logo_mariachi {
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+.text_mariachi h1{
+  font-weight: 300;
+  font-size: 60px;
+}
+.text_mariachi h2{
+  font-weight: 900;
+  font-size: 24px;
+}
+.text_mariachi h3{
+  font-weight: 900;
+  font-size: 24px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  color: #45D9E1;
+  text-align: center;
+}
+.text_mariachi p{
+  font-weight: 300;
+  font-size: 26px;
+}
+.logos img {
+  width: 100%;
+  display: block;
+  margin: auto;
+}
+footer {
+  background-color: #101F2D;
+  color: #516980;
+  text-align: center;
+  bottom: 0;
+  position:fixed;
+  width: 100%;
+}
+footer h6 {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-family: 'Montserrat';
+  font-size: 12px;
+  display: inline-block;
+}
+#derechos{
+  text-align: center;
+}
+#correo {
+  float: right;
+  margin-right: 10px;
+}
+</style>
